567a2edbe9322fa6c39deaf637c5cbef618bc092
mingpepe <mingpepemath@gmail.com>
2023-12-22 20:45:06 +0800

    feat: get target path from argument

--- server/server.js
+++ server/server.js
@@ -2,8 +2,16 @@ const WebSocket = require('ws');
 const fs = require('fs');
 
 const wss = new WebSocket.Server({ port: 8080 });
-
-fs.watch('..\\extension\\', { recursive: true }, (eventType, filename) => {
+if (process.argv.length != 3) {
+  console.log('Usage: node server.js targetPath');
+  process.exit();
+}
+const targetPath = process.argv[2];
+if (!fs.existsSync(targetPath)) {
+  console.log(`${targetPath} does not exist`);
+  process.exit();
+}
+fs.watch(targetPath, { recursive: true }, (eventType, filename) => {
   if (eventType === 'change') {
     console.log('file changed');
     wss.clients.forEach(client => {
