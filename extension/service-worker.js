4d9e26906a18ee91fabe7e48b8d91e528ed297e3
mingpepe <mingpepemath@gmail.com>
2023-12-23 16:55:14 +0800

    feat: reload extension by name read from manifest.json

--- extension/service-worker.js
+++ extension/service-worker.js
@@ -1,8 +1,8 @@
 console.log('code in service-worker.js');
 chrome.runtime.onInstalled.addListener(() => {
     const ws = new WebSocket('ws://localhost:8080');
-    ws.onmessage = () => {
-        reload();
+    ws.onmessage = (msg) => {
+        reload(msg.data);
     };
     setInterval(() => {
         ws.send('keep alive');
@@ -11,32 +11,56 @@ chrome.runtime.onInstalled.addListener(() => {
 
 chrome.commands.onCommand.addListener((command) => {
     if (command === "reload-extensions") {
-        reload();
+        reloadAll();
     }
 });
 
 let lock = false;
 
-async function reload() {
+async function reload(name) {
     if (lock) {
         return;
     }
     lock = true;
-    console.log('Reload all installed extensions(except self)');
+    console.log(`Reload extension with name = ${name}`);
+    const extensions = await chrome.management.getAll();
+    let found = false;
+    for (let i = 0; i < extensions.length; i++) {
+        if (extensions[i].name === name) {
+            console.log('Find the target extension, reload it');
+            found = true;
+            const id = extensions[i].id;
+            await chrome.management.setEnabled(id, false);
+            await chrome.management.setEnabled(id, true);
+            break;
+        }
+    }
+    if (!found) {
+        console.log(`Extension with name = ${name} not found`);
+    }
+    setTimeout(() => {
+        lock = false;
+    }, 1000);
+}
+
+async function reloadAll() {
+    if (lock) {
+        return;
+    }
+    lock = true;
+    console.log('Reload all extensions');
     const self = await chrome.management.getSelf();
     const extensions = await chrome.management.getAll();
-    console.log(self);
-    console.log(extensions);
     for (let i = 0; i < extensions.length; i++) {
-        const id = extensions[i].id;
-        if (id === self.id) {
+        if (extensions[i].id === self.id) {
             console.log('Skip my self');
         } else {
-            console.log(`Reload extension: ${id}`);
+            const id = extensions[i].id;
             await chrome.management.setEnabled(id, false);
             await chrome.management.setEnabled(id, true);
         }
     }
+
     setTimeout(() => {
         lock = false;
     }, 1000);
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const WebSocket = require('ws');
 const fs = require('fs');
+const path = require('path');
 
 const wss = new WebSocket.Server({ port: 8080 });
 if (process.argv.length != 3) {
@@ -11,12 +12,31 @@ if (!fs.existsSync(targetPath)) {
   console.log(`${targetPath} does not exist`);
   process.exit();
 }
+
+const manifestPath = path.join(targetPath, 'manifest.json');
+if (!fs.existsSync(manifestPath)) {
+  console.log(`${manifestPath} does not exist`);
+  process.exit();
+}
+
+const data = fs.readFileSync(manifestPath, 'utf8');
+const json = JSON.parse(data);
+const name = json.name;
+
+if (!name) {
+  console.log(`${manifestPath} does not contain 'name'`);
+  process.exit();
+}
+
 fs.watch(targetPath, { recursive: true }, (eventType, filename) => {
   if (eventType === 'change') {
-    console.log('file changed');
+    console.log(`file changed: ${filename}`);
+    if (filename === 'manifest.json') {
+      console.log('Warn: if you change name of the extension, this hot reload will not work');
+    }
     wss.clients.forEach(client => {
       if (client.readyState === WebSocket.OPEN) {
-        client.send('File changed: ' + filename);
+        client.send(name);
       }
     });
   }
